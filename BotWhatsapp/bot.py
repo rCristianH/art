45a05224c0e045df6ab22d3aeb4b4ab24df6a98e
Cristian Ramirez <ccrhdbj@outlook.com>
2022-08-02 18:29:33 -0500

    commit

--- BotWhatsapp/bot.py
+++ BotWhatsapp/bot.py
@@ -2,10 +2,11 @@ import pywhatkit
 import time
 from datetime import date, datetime
 
+
 seconds = time.time() + 60
 date = datetime.fromtimestamp(seconds)
 
-pywhatkit.sendwhatmsg("+573133035038", "Hola", date.hour, date.minute)
+pywhatkit.sendwhatmsg("+57", "Hola", date.hour, date.minute)
 
 """ time.sleep(5)
  """""" pywhatkit.sendwhats_image("","Image") """
